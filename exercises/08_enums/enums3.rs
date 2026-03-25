a963de2a7ef4141b05c4ecff5f4f074866d7fef9
Nat Chapman <nate@nathanwchapman.com>
2026-03-24 23:07:14 -0700

    finish enums and onto strings

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -47,7 +47,11 @@ impl State {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
         match message {
-            Message::Resize { width, height } =>
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(mess) => self.echo(mess),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+            Message::Quit => self.quit(),
         }
     }
 }
