9e6a423b104a969110a5ffaa1e28b44f2b4e1488
Nat Chapman <nate@nathanwchapman.com>
2026-03-28 08:59:20 -0700

    finish strings!

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,19 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let suffix: &str = " world!";
+    [input, suffix].concat()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let pat: &str = "cars";
+    let rep: &str = "balloons";
+    input.replace(pat, rep)
 }
 
 fn main() {
